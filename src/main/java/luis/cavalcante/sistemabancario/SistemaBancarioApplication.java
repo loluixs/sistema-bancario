0843948bb207a95934f9cb7ae86424705e09f3e8
clean <cleanmec@gmail.com>
2024-02-29 14:19:50 -0300

    MongoRepositories

--- src/main/java/luis/cavalcante/sistemabancario/SistemaBancarioApplication.java
+++ src/main/java/luis/cavalcante/sistemabancario/SistemaBancarioApplication.java
@@ -2,12 +2,17 @@ package luis.cavalcante.sistemabancario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@EnableMongoRepositories
+
 public class SistemaBancarioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaBancarioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBancarioApplication.class, args);
+    }
 
 }
