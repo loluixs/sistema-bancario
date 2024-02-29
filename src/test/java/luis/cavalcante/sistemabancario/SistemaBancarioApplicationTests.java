d936203770121b8423810075a6639815f9eb095d
clean <cleanmec@gmail.com>
2024-02-29 11:32:11 -0300

    Sistema Bancário

---
+++ src/test/java/luis/cavalcante/sistemabancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package luis.cavalcante.sistemabancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
